5a33cff4b7e521b6895e1702dffdc2835cff069c
Stefkoo1 <stefan.lackovic@stud.fh-campuswien.ac.at>
2024-06-08 16:51:40 +0200

    added Layout for profile.html

--- public/js/modules/profile.js
+++ public/js/modules/profile.js
@@ -21,3 +21,45 @@ document.addEventListener('DOMContentLoaded', () => {
     }    
     populateSidebar();
 })
+
+document.getElementById('editButton').addEventListener('click', function() {
+    document.getElementById('aboutText').style.display = 'none';
+    document.getElementById('aboutTextarea').style.display = 'block';
+    document.getElementById('aboutTextarea').value = document.getElementById('aboutText').innerText;
+    document.getElementById('fileInputContainer').style.display = 'block';
+    document.getElementById('editButton').style.display = 'none';
+    document.getElementById('saveButton').style.display = 'inline-block';
+    document.getElementById('cancelButton').style.display = 'inline-block';
+});
+
+document.getElementById('saveButton').addEventListener('click', function() {
+    const aboutText = document.getElementById('aboutTextarea').value;
+    document.getElementById('aboutText').innerText = aboutText;
+    document.getElementById('aboutText').style.display = 'block';
+    document.getElementById('aboutTextarea').style.display = 'none';
+    document.getElementById('fileInputContainer').style.display = 'none';
+    document.getElementById('editButton').style.display = 'inline-block';
+    document.getElementById('saveButton').style.display = 'none';
+    document.getElementById('cancelButton').style.display = 'none';
+});
+
+document.getElementById('cancelButton').addEventListener('click', function() {
+    document.getElementById('aboutText').style.display = 'block';
+    document.getElementById('aboutTextarea').style.display = 'none';
+    document.getElementById('fileInputContainer').style.display = 'none';
+    document.getElementById('editButton').style.display = 'inline-block';
+    document.getElementById('saveButton').style.display = 'none';
+    document.getElementById('cancelButton').style.display = 'none';
+});
+
+document.getElementById('uploadProfilePic').addEventListener('change', function(event) {
+    const fileInput = event.target;
+    const fileName = fileInput.files[0].name;
+    document.getElementById('file-name').innerText = fileName;
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        document.getElementById('profilePic').src = e.target.result;
+    };
+    reader.readAsDataURL(fileInput.files[0]);
+});
