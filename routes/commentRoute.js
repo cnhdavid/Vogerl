ce4aca72e525a1776869045a451a1412ba388d14
Jasmeet <jasmeet@gmx.at>
2024-06-23 20:41:40 +0200

    ...

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const createPool = require('../db');
-c
 const authenticateToken = require('../authenticate');
 const pool = createPool.createPool();
 
