f2508162a1f3967e4022ba0cf72c96c5c6abe835
Jasmeet <jasmeet@gmx.at>
2024-06-26 09:53:42 +0200

    ...

--- db.js
+++ db.js
@@ -13,7 +13,6 @@ function createPool() {
     poolInstance = new Pool({
       connectionString: connectionString,
       max: 20, // Adjust the max number of clients in the pool
-      idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
       connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
     });
 
